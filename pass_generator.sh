18049891905c433d5ec03e56c0162accbe175ffa
Baytuch <admin@my-services.km.ua>
2021-10-05 08:18:49 +0300

    Add rand len

--- pass_generator.sh
+++ pass_generator.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 dd if=/dev/random of=rand_file count=512
-md5sum rand_file | cut -b 1-18 > pass.txt
+md5sum rand_file | cut -b 1-$((18 + $(dd if=/dev/random count=1 bs=1 status=none | od -An -i) / 51)) > pass.txt
 rm -f rand_file
