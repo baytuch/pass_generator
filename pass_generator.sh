b8edb93c5f896591c47de0b483527ad266d1925a
Baytuch <admin@my-services.km.ua>
2021-10-05 08:23:06 +0300

    Increased seed to 1024

--- pass_generator.sh
+++ pass_generator.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-dd if=/dev/random of=rand_file count=512 status=none
+dd if=/dev/random of=rand_file count=1024 status=none
 md5sum rand_file | cut -b 1-$((18 + $(dd if=/dev/random count=1 bs=1 status=none | od -An -i) / 51)) > pass.txt
 rm -f rand_file
